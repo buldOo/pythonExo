42d20ebff560741724870e0c8fa1d356cbc74275
killian <72389975+buldOo@users.noreply.github.com>
2022-06-27 15:03:37 +0200

    add docstring

--- python/functions.py
+++ python/functions.py
@@ -1,4 +1,9 @@
 def ascii_to_binary(values_to_convert):
+    """ A function ton convert ascii number into binary number
+
+    :param values_to_convert: An array of value to convert
+    :return: An array of binary values
+    """
     print(len(values_to_convert))
     binarys = []
     for value_to_convert in values_to_convert:
@@ -9,4 +14,3 @@ def ascii_to_binary(values_to_convert):
 def convert_char_to_ascii(char_to_convert):
     char_ascii = ord(char_to_convert)
     return char_ascii
-
