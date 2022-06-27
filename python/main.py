7d9279ef46597d780db90dc2ff8eeefe493ecb18
killian <72389975+buldOo@users.noreply.github.com>
2022-06-27 10:45:42 +0200

    exo1

--- python/main.py
+++ python/main.py
@@ -11,8 +11,13 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    annee = int(input("Entrez l annee a verifier:"))
-    if (annee % 4 == 0 and annee % 100 != 0 or annee % 400 == 0):
-        print("L'annee est une annee bissextile!")
-    else:
-        print("L'annee n'est pas une annee bissextile!")
+    i = 1
+    while i == 1:
+        annee = int(input("Entrez l annee a verifier:"))
+        if (annee == 0000):
+            print("A la prochaine")
+            i = 0
+        elif (annee % 4 == 0 and annee % 100 != 0 or annee % 400 == 0):
+            print("L'annee est une annee bissextile!")
+        else:
+            print("L'annee n'est pas une annee bissextile!")
