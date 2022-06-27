31546194cd0f55a01133e15b2a5f54b70f6cf19c
Paul Geneve <geneve.paul1295@gmail.com>
2022-06-27 14:03:46 +0200

    add function chart to ascii

--- python/functions.py
+++ python/functions.py
@@ -1,3 +1,7 @@
 def ascii_to_binary(value_to_convert):
     binary = format(value_to_convert, "b")
     return(binary)
+
+def convert_char_to_ascii(char_to_convert):
+    char_ascii = ord(char_to_convert)
+    return char_ascii
