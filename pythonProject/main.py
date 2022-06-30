0ef900b93127a39038a3b63d634bd4f9c2144520
Paul Geneve <geneve.paul1295@gmail.com>
2022-06-30 16:37:49 +0200

    connect DB

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,6 +1,25 @@
 import functions
 import psutil
 import logging
+import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+bucket = "influxdb"
+org = "PKMM"
+token = "KtpO_uTOFJZDeqKlmHQkKFHJwYhC5RFZmTXvdrCSn8VL-EjY9N0LLNiQ4SJXX_p5J-oKpKoW-RwWagOgpe-jvg=="
+# Store the URL of your InfluxDB instance
+url="http://10.57.29.248:8086"
+
+client = influxdb_client.InfluxDBClient(
+   url=url,
+   token=token,
+   org=org
+)
+
+write_api = client.write_api(write_options=SYNCHRONOUS)
+
+p = influxdb_client.Point("Measurment Paul").tag("location", "Paris").field("temperature", 25.3)
+write_api.write(bucket=bucket, org=org, record=p)
 
 logging.basicConfig(filename='stat.log', encoding='utf-8', level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
